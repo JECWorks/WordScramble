38567a86d7ac25472627bc23621d338d91eada4c
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-09-25 19:14:34 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Jason Cox on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
