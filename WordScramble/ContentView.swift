2182b50e4a4acec70bccffabb64c6caad22399c1
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-09-29 22:24:39 -0400

    Loading resources from your app bundle

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    let people = ["Finn", "Leia", "Luke", "Rey"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List {
+            Section("Section 1") {
+                Text("Static Row 1")
+                Text("Static Row 2")
+            }
+            Section("Section 2") {
+                ForEach(0..<5) {
+                    Text("Dynamic Row \($0)")
+                }
+            }
+        }
+        List(people, id: \.self) {
+            Text("Person: \($0)")
         }
         .padding()
+    }
+    func testBundles() {
+        if let fileURL = Bundle.main.url(forResource: "somefile", withExtension: "txt") {
+            // we found the file in our bundle!
+            if let fileContents = try? String(contentsOf: fileURL) {
+                // we loaded the file into a string
+            }
+        }
     }
 }
 
